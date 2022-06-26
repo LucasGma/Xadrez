d1c747da4a3db8c59bed8f38098082431f24faa4
Lucas.almeida <lucasgma320@yahoo.com.br>
2022-06-25 21:24:58 -0300

    Criada classe posicao

--- Console-xadrez/Program.cs
+++ Console-xadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Console_xadrez
 {
@@ -10,6 +11,10 @@ namespace Console_xadrez
     {
         static void Main(string[] args)
         {
+            Posicao p = new Posicao(3,4);
+
+            Console.WriteLine(p.ToString());
+            Console.ReadLine();
         }
     }
 }
---
+++ Console-xadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace tabuleiro
+{
+    internal class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao(int coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public override string ToString()
+        {
+            return linha + "," + coluna;
+        }
+    }
+}
